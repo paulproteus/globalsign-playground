db77539194ccb9fadac548902caa095ba23d3e32
Asheesh Laroia <asheesh@asheesh.org>
2015-07-30 18:56:45 -0700

    Attempt to make a PVOrder

--- funtimes.py
+++ funtimes.py
@@ -21,9 +21,30 @@ def get_mssl_domains():
     args.QueryRequestHeader.AuthToken.Password = _get_password()
     return suds_client.service.GetMSSLDomains(args)
 
-def check_domain_in_list(domain):
+def get_mssl_domain_info(domain):
     x = get_mssl_domains()
     for domain_details in x.SearchMsslDomainDetails.SearchMsslDomainDetail:
         if domain_details.MSSLDomainName == domain:
-            return True
-    return False
+            return {
+                'MSSLDomainID': domain_details.MSSLDomainID,
+                'MSSLProfileID': domain_details.MSSLProfileID,
+            }
+    return None
+
+def sign_csr():
+    domain_info = get_mssl_domain_info('sandcats.io')
+
+    args = suds_client.factory.create('PVOrder')
+    args.Request.OrderRequestHeader.AuthToken.UserName = 'PAR03383_sandstorm'
+    args.Request.OrderRequestHeader.AuthToken.Password = _get_password()
+    args.Request.OrderRequestParameter.ProductCode = 'PV'
+    args.Request.OrderRequestParameter.OrderKind = 'New'
+    args.Request.OrderRequestParameter.ValidityPeriod.Months = 1  # FIXME
+    args.Request.OrderRequestParameter.CSR = open('csr/just-testing.sandcats.io.csr').read()
+    args.Request.MSSLDomainID = domain_info['MSSLDomainID']
+    args.Request.MSSLProfileID = domain_info['MSSLProfileID']
+    args.Request.ContactInfo.FirstName = 'Bond'
+    args.Request.ContactInfo.LastName = 'James Bond'
+    args.Request.ContactInfo.Phone = '+1 585-555-1234'
+    args.Request.ContactInfo.Email = 'bond@example.com'
+    return suds_client.service.PVOrder(args)
